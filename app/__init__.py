cf55addccb75aa221018801464f92e00315bf851
VladimirPod <podkhvatilinvv@gmail.com>
2026-06-13 20:48:50 +0400

    Added user registration via Blueprint auth. Password hashing in the model, field validation, and strict email format checking via regular expressions have been configured. Added JS for hiding alerts on click.

--- app/__init__.py
+++ app/__init__.py
@@ -1,16 +1,27 @@
-from flask import Flask 
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy 
 
+db = SQLAlchemy()
 
 def create_app():
     app = Flask(__name__)
     
-     # Импортируем созданный Blueprint
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///project.db'
+    app.config['SECRET_KEY'] = 'admin'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    
+    db.init_app(app)
+    # Импортируем созданный Blueprint
     from app.routes.auth import auth_bp
-    
-    
     # Регистрируем его в приложении. 
     # url_prefix добавит '/auth' ко всем ссылкам этого блюпринта автоматически.
     app.register_blueprint(auth_bp, url_prefix='/auth')
+
+    with app.app_context():
+        
+        from app import models
+        
+        db.create_all()
     
     @app.route('/')
     def home():
--- app/routes/auth.py
+++ app/routes/auth.py
@@ -1,4 +1,12 @@
-from flask import Blueprint, url_for, render_template
+from flask import Blueprint, url_for, render_template, request, redirect, flash
+from app.models import User
+from app import db
+# ИМПОРТ ЗДЕСЬ: импортируем класс User из файла моделей
+from app.models import User 
+import re
+
+# Регулярное выражение для строгой проверки email (обязательно наличие точки и домена)
+EMAIL_REGEX = r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$'
 
 auth_bp = Blueprint('auth', __name__, template_folder='templates')
 
@@ -6,7 +14,41 @@ auth_bp = Blueprint('auth', __name__, template_folder='templates')
 
 @auth_bp.route('/register', methods=['GET', 'POST'])
 def register():
+    if request.method == 'POST':
+        username = request.form.get('username')
+        email = request.form.get('email')
+        password = request.form.get('password')
+        confirm_password = request.form.get('confirm_password')
+        
+        if not (username and email and password and confirm_password):
+            flash('Все поля должны быть заполнены!')
+            return redirect(url_for('auth.register'))
+        
+        # проверка с точным текстом ошибки
+        if not re.match(EMAIL_REGEX, email):
+            flash('Пожалуйста, ведите корректный email!')
+            return redirect(url_for('auth.register'))
+        
+        if password != confirm_password:
+            flash('Пароли не совпадают!')
+            return redirect(url_for('auth.register'))
+    
+        if User.query.filter(User.email == email).first():
+            flash('Пользователь с такой почтой уже существует!')
+            return redirect(url_for('auth.register'))
+        
+        new_user = User(username=username, email=email)
+        new_user.set_password(password)
+        
+        db.session.add(new_user)
+        db.session.commit()
+        
+        flash('Регистрация успешна! Войдите в аккаунт.')        
+        return redirect(url_for('auth.login'))
+    
     return render_template('auth/register.html')
+    
+    
 
 @auth_bp.route('/login', methods=['GET', 'POST'] )
 def login():
